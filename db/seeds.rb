9ce4a1ea36d2a849be24ab7e57e5c5a5cb78d50c
Haider Zulfiqar <haiderbhinder10@gmail.com>
2022-12-06 03:10:34 +0500

    Model/Views/Controller for User and Messages with association between them and login and home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username:"hadi", password:"12345")
+User.create(username:"hamza", password:"12345")
+User.create(username:"usman", password:"12345")
+User.create(username:"ali", password:"12345")
+User.create(username:"umer", password:"12345")
+
+
+Message.create(body: "ohhh new gc", user_id:2)
+Message.create(body: "Hello everyone", user_id:3)
+Message.create(body: "Hello hamza", user_id:1)
+Message.create(body: "hi guys", user_id:5)
+
