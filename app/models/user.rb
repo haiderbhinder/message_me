125ba5fccea9ead2329609f2b3c7a80b06312d66
Haider Zulfiqar <haiderbhinder10@gmail.com>
2022-12-06 21:28:36 +0500

    add messages from ui

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
+  has_many :messages   
   has_secure_password
   validates :username, presence: true, length: { minimum: 3, maximum: 15 },
-            uniqueness: { case_sensitive: false }
+            uniqueness: { case_sensitive: false }       
 end
